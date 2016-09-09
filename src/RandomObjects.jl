750babdc9ebec865cdfd040a05df04ffb0f103f7
Jan Weidner <jw3126@gmail.com>
2016-09-09 15:02:51 +0200

    tuples

--- src/RandomObjects.jl
+++ src/RandomObjects.jl
@@ -3,18 +3,25 @@ module RandomObjects
 
 export random
 
-random(::Type{String}; l::Integer=rand(2:10)) = randstring(l)
-random(T, n::Integer) = T[random(T) for _ in 1:n]
-random{T}(::Type{Vector{T}}; l::Integer=rand(0:20)) = random(T,l)
-random{T <: Number}(::Type{T}) = rand(T)
+@inline random(::Type{String}; l::Integer=rand(2:10)) = randstring(l)
+@inline random(T, n::Integer) = T[random(T) for _ in 1:n]
+@inline random{T}(::Type{Vector{T}}; l::Integer=rand(0:20)) = random(T,l)
+@inline random{T <: Number}(::Type{T}) = rand(T)
 
-function random_impl{T}(::Type{T})
+function random_from_fields_impl{T}(::Type{T})
     fieldtypes = T.types
     random_types = [:(random($S)) for S in fieldtypes]
     Expr(:call, T, random_types...)
 end
 
-@generated random{T}(::Type{T}) = random_impl(T)
+function random_tuple_impl{T <: Tuple}(::Type{T})
+    fieldtypes = T.parameters
+    random_types = [:(random($S)) for S in fieldtypes]
+    Expr(:tuple, random_types...)
+end
 
+
+@generated random{T}(::Type{T}) = random_from_fields_impl(T)
+@generated random{T <: Tuple}(::Type{T}) = random_tuple_impl(T)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -12,8 +12,13 @@ immutable B
     as::Vector{A}
 end
 
+type C{S}
+    ab::Tuple{A, B, S}
+end
+
+
 @testset "Test random(T)::T" begin
-    for T in [Int, String, A, B]
+    for T in [Int, String, A, B, C{Int}]
         n = rand(0:5)
         @test typeof(@inferred random(T)) == T
         @test length(@inferred random(T, n)) == n
